da192c7c5f7389d05ec99b3b7a8b9066be6a0096
Eric Schrag <eschrag@gmail.com>
2014-10-22 21:40:10 -0400

    Add basic UserDetailActivity

---
+++ app/src/main/java/net/ericschrag/ud_demo/GithubUserDetailActivity.java
@@ -0,0 +1,15 @@
+package net.ericschrag.ud_demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GithubUserDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github_user_detail);
+    }
+
+}
