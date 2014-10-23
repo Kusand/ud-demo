929779ef4504ea114c174f2b2d83a0535143d76d
Eric Schrag <eschrag@gmail.com>
2014-10-22 21:36:32 -0400

    Make sure to tell Github API what version we are expecting

--- app/src/main/java/net/ericschrag/ud_demo/data/GithubService.java
+++ app/src/main/java/net/ericschrag/ud_demo/data/GithubService.java
@@ -5,8 +5,10 @@ import net.ericschrag.ud_demo.data.model.GithubUser;
 import java.util.List;
 
 import retrofit.http.GET;
+import retrofit.http.Headers;
 
 public interface GithubService {
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("/users")
     public List<GithubUser> getUsers();
 }
